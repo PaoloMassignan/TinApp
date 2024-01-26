dff6d181d9cab4a81386241821072024e700c91d
Paolo Massignan <paolomassignan@gmail.com>
2024-01-26 22:38:23 +0100

    Gestione delle immagini

--- TinApp2/ImageService.cs
+++ TinApp2/ImageService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Maui.Controls;
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Controls;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,9 +22,48 @@ namespace TinApp2
             }
         }
 
-        private Dictionary<string, ImageSource?> _cache = new Dictionary<string, ImageSource?>();
 
-        public string GetImage(string path)
+        private string GetPrefix(string x)
+        {
+            int firstIndex = x.IndexOf("Exercises.") + "Exercises.".Length;
+            int secondIndex = x.IndexOf(".", firstIndex);
+            return x.Substring(firstIndex, secondIndex - firstIndex);
+        }
+
+        private string GetPath(string x)
+        {
+            int firstIndex = x.LastIndexOf(".");
+            int secondIndex = x.LastIndexOf(".", firstIndex - 1) + 1;
+            return x.Substring(secondIndex);
+        }
+
+
+        private ImageService()
+        {
+            Assembly a = Assembly.GetExecutingAssembly();
+            var exercises = new List<string>();
+            foreach (var resource in a.GetManifestResourceNames())
+            {
+                if (!resource.Contains("Exercises"))
+                    continue;
+                exercises.Add(resource);
+            }
+            exercises.GroupBy(x => GetPrefix(x)).ToList().ForEach(x => _imageSources.Add(x.Key, x.ToList()));
+        }
+
+        private Dictionary<string, List<string>> _imageSources = new Dictionary<string, List<string>>();
+
+        public int Count => _imageSources.Count;
+
+        public List<string> GetCategories()
+        {
+            return _imageSources.Keys.ToList();
+        }
+        public List<string> GetImageOfCategory(string key)
+        {
+            return _imageSources[key].Select(x => GetImage(x)).ToList() ;
+        }
+        private string GetImage(string path)
         {
             //if (_cache.ContainsKey(path))
             //    return _cache[path];
@@ -35,6 +75,8 @@ namespace TinApp2
             imageSource = string.Format("data:image/png;base64,{0}", imageSource);
             return imageSource;
         }
+
+
 
 
     }
--- TinApp2/MauiProgram.cs
+++ TinApp2/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using MudBlazor.Services;
 
 namespace TinApp2
 {
@@ -15,6 +16,7 @@ namespace TinApp2
                 });
 
             builder.Services.AddMauiBlazorWebView();
+            builder.Services.AddMudServices();
 
 #if DEBUG
     		builder.Services.AddBlazorWebViewDeveloperTools();
